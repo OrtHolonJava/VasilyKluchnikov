f7a2f2b1e7dfaaa37debf5ecd9ce7f78ff57cf22
dividded <dividded@gmail.com>
2017-11-15 22:24:42 +0200

    Added .gitignore, some basic folder structure, empty main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package main.java;
+
+/**
+ * Created by divided on 15.11.2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
